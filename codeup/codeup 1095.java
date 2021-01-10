35d7ece2277bdaee564f551fcfe63be4db569ac2
repl.it user <replituser@example.com>
2021-01-10 15:11:27 +0000

    codeup 1095 update

--- codeup/codeup 1095.java
+++ codeup/codeup 1095.java
@@ -2,21 +2,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
-	public static void main(String[] args) {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			int n = Integer.parseInt(br.readLine());
-			String[] k = br.readLine().split(" ");
-			int min = 24;
-			if(n > 0 && n <= 10000){
-				for (int i = 0; i < k.length; i++) {
-					min = min > Integer.parseInt(k[i]) ? Integer.parseInt(k[i]) : min;
-				}
-			}
-			System.out.println(min);
-		} catch (NumberFormatException | IOException e) {
-			// TODO: handle exception
-		}
-	}
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        String [] arr = br.readLine().split(" ");
+        int min = 24;
+
+        if(n > 0 && n <= 10000){
+            for(int i = 0; i < arr.length; i++){
+                min = min > Integer.parseInt(arr[i])?Integer.parseInt(arr[i]) : min;
+            }
+        }
+        System.out.println(min);
+    }
 }
